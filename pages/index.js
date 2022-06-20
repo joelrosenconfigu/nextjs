706cef6da02fb1587dcaa24766fd13de433c7bbe
joelrosenconfigu <107347870+joelrosenconfigu@users.noreply.github.com>
2022-06-20 16:21:11 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+const location = process.env.LOCATION;
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -13,7 +15,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://minecraft.fandom.com/wiki/Herobrine">${location}</a>
         </h1>
 
         <p className={styles.description}>
