3a82ce12795faff58b83f1051abe8759f1355a54
joelrosenconfigu <107347870+joelrosenconfigu@users.noreply.github.com>
2022-06-20 16:24:42 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://minecraft.fandom.com/wiki/Herobrine">${location}</a>
+          Welcome to ${location} <a href="https://minecraft.fandom.com/wiki/Herobrine">hi</a>
         </h1>
 
         <p className={styles.description}>
